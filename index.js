0e6dcefc4f9fb7c2f7b13bd8593a3c654218bdfd
Md Sabbir Khan <mdsabbirkhan1972@gmail.com>
2024-06-04 00:00:02 +0600

    jwt implement

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 const express = require('express');
 const cors = require('cors');
-
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
+const cookieParser = require('cookie-parser');
 const app = express();
 const port = process.env.PORT || 5000;
 
-// middleware
+var jwt = require('jsonwebtoken');
+
+// middlewares
 app.use(
   cors({
     origin: ['http://localhost:5173'],
     credentials: true,
   })
 );
-
-// middle ware
 app.use(express.json());
 app.use(cookieParser());
 
@@ -31,6 +31,28 @@ const client = new MongoClient(uri, {
 
 // create own// middlewares
 
+const logger = (req, res, next) => {
+  console.log('Log Information', req.method, req.url);
+  next();
+};
+
+const verifyToken = (req, res, next) => {
+  const token = req?.cookies?.token;
+  console.log('verify token', token);
+  if (!token) {
+    return res.status(401).send({ message: 'unauthorized user' });
+  }
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
+    if (error) {
+      return res.status(401).send({ message: 'unauthorized access' });
+    }
+
+    req.user = decoded;
+    next();
+  });
+};
+
 async function run() {
   try {
     const carDoctorsCollection = client
@@ -38,6 +60,31 @@ async function run() {
       .collection('Services');
 
     const bookingCollection = client.db('CarDoctorsDB').collection('Bookings');
+
+    // jwt section implement
+
+    app.post('/jwt', logger, async (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: '1h',
+      });
+
+      res
+        .cookie('token', token, {
+          httpOnly: true,
+          secure: true,
+          sameSite: 'none',
+        })
+        .send({ success: true });
+    });
+
+    // if user logout clear the cookies api
+    app.post('/logout', (req, res) => {
+      const user = req.body;
+      console.log('logout user', user);
+      res.clearCookie('token', { maxAge: 0 }).send({ success: true });
+    });
 
     // get one services by id
     app.get('/services/:id', async (req, res) => {
@@ -51,7 +98,7 @@ async function run() {
     });
 
     // get all services
-    app.get('/services', async (req, res) => {
+    app.get('/services', logger, async (req, res) => {
       const cursor = carDoctorsCollection.find();
       const services = await cursor.toArray();
       res.send(services);
@@ -62,8 +109,14 @@ async function run() {
 
     // get all data for specific user from db
 
-    app.get('/bookings', verifyToken, async (req, res) => {
+    app.get('/bookings', logger, verifyToken, async (req, res) => {
       // console.log(req.query.email);
+      // console.log(req.cookies);
+      // console.log('token owner', req.user);
+
+      if (req.query.email !== req.user.email) {
+        return res.status(403).send({ message: 'forbidden access' });
+      }
 
       // for get specific order booking
       let query = {};
